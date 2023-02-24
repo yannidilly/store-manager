925c1c6fac3bc8ea625270e35369aa588448046e
Yanni <yannidilly.prog@gmail.com>
2023-02-24 20:49:31 -0300

    corrige formato de escrita de referencia ao banco de dados

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -3,14 +3,14 @@ const connection = require('./connection');
 
 const findAll = async () => {
   const [result] = await connection.execute(
-    'SELECT * FROM products',
+    'SELECT * FROM StoreManager.products',
   );
   return camelize(result);
 };
 
 const findById = async (id) => {
   const [result] = await connection.execute(
-    'SELECT * FROM products WHERE id = ?',
+    'SELECT * FROM StoreManager.products WHERE id = ?',
     [id],
   );
   return camelize(result);
