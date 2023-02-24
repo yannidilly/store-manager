517f670a35bf66764df5190cee951257ef7606ad
Yanni <yannidilly.prog@gmail.com>
2023-02-24 18:31:25 -0300

    integra rota /products no app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const productsRouters = require('./routers/products.router');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/products/', productsRouters);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
