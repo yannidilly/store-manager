0386722359bd38daf8c5e7d54a388b6c4b97bdae
Yanni <yannidilly.prog@gmail.com>
2023-02-24 16:05:55 -0300

    cria função de busca de todos os produtos na camada model

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,3 +1,16 @@
-const findAll = async () => { };
+const camelize = require('camelize');
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM products',
+  );
+  return camelize(result);
+};
 
 const findById = async (id) => { };
+
+module.exports = {
+  findAll,
+  findById,
+};
