9ee7896c12d8d57bcc5e15d28ec8bb6c53b9e74b
Yanni <yannidilly.prog@gmail.com>
2023-02-24 17:44:01 -0300

    cria funções para listar todos os produtos e apenas um pelo id na camada de controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,20 @@
 const productsService = require('../services/products.service');
+const { sucessStatus, mapStatusError } = require('../utils/mapStatus');
 
 const findAll = async (_req, res) => {
   const { type, message } = await productsService.findAll();
-  if ()
+  if (type) return res.status(mapStatusError.mapError(type)).json(message);
+  return res.status(sucessStatus).json(message);
+};
+
+const findById = async (req, res) => {
+  const id = req.params;
+  const { type, message } = await productsService.findById(id);
+  if (type) return res.status(mapStatusError.mapError(type)).json(message);
+  return res.status(sucessStatus).json(message);
+};
+
+module.exports = {
+  findAll,
+  findById,
 };
