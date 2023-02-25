51c4ba26af0d2999f90d176c8bc6b77a89fcb76d
Yanni <yannidilly.prog@gmail.com>
2023-02-24 22:26:12 -0300

    corrige warning no console removendo variavel de ambiente desnecessaria

--- src/models/connection.js
+++ src/models/connection.js
@@ -4,7 +4,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  waitForConnection: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
