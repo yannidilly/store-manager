265cca50f812fbb6509c4ced5b07d88d2aa5c76f
Yanni <yannidilly.prog@gmail.com>
2023-02-25 19:23:35 -0300

    cria mock dos produtos para camada model

---
+++ tests/unit/models/mocks/products.model.mock.js
@@ -0,0 +1,24 @@
+const allProducts = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+  {
+    "id": 2,
+    "name": "Traje de encolhimento"
+  },
+  {
+    "id": 3,
+    "name": "Escudo do Capitão América"
+  }
+];
+
+const productId1 = {
+  "id": 1,
+  "name": "Martelo de Thor"
+}
+
+module.exports = {
+  allProducts,
+  productId1,
+};
