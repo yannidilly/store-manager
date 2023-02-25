f54794c1c77ef9275e600924c16de1f0d7a6cb30
Yanni <yannidilly.prog@gmail.com>
2023-02-25 19:31:27 -0300

    cria testes para função productsModel.findAll()

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -1,5 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../src/models/connection');
+const productsModel = require('../../../src/models/products.model');
+
+const { allProducts } = require('./mocks/products.model.mock');
+
 describe('Realiza testes nas funções do produto da camada model', () => {
-  test('Testa se função findAll retorna lista com todos os produtos', () => {
+  test('Testa se função findAll retorna lista com todos os produtos', async () => {
+    sinon.stub(connection, 'execute').resolves(allProducts);
+
+    const result = await productsModel.findAll();
+    expect(result).to.be.deep.equal(allProducts);
 
   });
 
