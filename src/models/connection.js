1404ac8ec0d13c0fc68a4ad17218a557fe18b455
Yanni <yannidilly.prog@gmail.com>
2023-02-24 21:11:46 -0300

    altera parametros da função createPool seguindo as variaveis de ambiente e removendo redundancias

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 3060,
-  user: 'root',
-  password: 'password',
-  database: 'store_manager_db',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  waitForConnection: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = connection;
