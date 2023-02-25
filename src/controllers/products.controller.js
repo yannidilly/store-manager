d64a01631d8695190b7c494d98b0bac085980d37
Yanni <yannidilly.prog@gmail.com>
2023-02-24 21:15:21 -0300

    corrige forma de chamar função mapStatusError

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,14 +3,14 @@ const { sucessStatus, mapStatusError } = require('../utils/mapStatus');
 
 const findAll = async (_req, res) => {
   const { type, message } = await productsService.findAll();
-  if (type) return res.status(mapStatusError.mapError(type)).json(message);
+  if (type) return res.status(mapStatusError(type)).json(message);
   return res.status(sucessStatus).json(message);
 };
 
 const findById = async (req, res) => {
   const id = req.params;
   const { type, message } = await productsService.findById(id);
-  if (type) return res.status(mapStatusError.mapError(type)).json(message);
+  if (type) return res.status(mapStatusError(type)).json(message);
   return res.status(sucessStatus).json(message);
 };
 
