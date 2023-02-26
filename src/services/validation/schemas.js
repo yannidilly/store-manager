78b18fa4bb7730be73ccff23565e1e7930416931
Yanni <yannidilly.prog@gmail.com>
2023-02-26 20:06:50 -0300

    cria schema de validação para nova venda

--- src/services/validation/schemas.js
+++ src/services/validation/schemas.js
@@ -1,11 +1,20 @@
 const joi = require('joi');
 
 const idSchema = joi.number().integer().min(1).required();
+
 const newProductSchema = joi.object({
   name: joi.string().min(5).required(),
 });
 
+const newSaleSchema = joi.array().items(
+  joi.object({
+    productId: idSchema,
+    quantity: idSchema,
+  }),
+);
+
 module.exports = {
   idSchema,
   newProductSchema,
+  newSaleSchema,
 };
