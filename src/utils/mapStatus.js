922fd61c89aabe23acb3b58406e1e38acb50e5c0
Yanni <yannidilly.prog@gmail.com>
2023-02-26 11:20:12 -0300

    cria novos status

--- src/utils/mapStatus.js
+++ src/utils/mapStatus.js
@@ -1,8 +1,11 @@
 const sucessStatus = 200;
+const successfullCreationStatus = 201;
 
 const errorStatusMap = {
   INVALID_VALUE: 400,
   PRODUCT_NOT_FOUND: 404,
+  NAME_IS_REQUIRED: 400,
+  NAME_LENGTH_ERROR: 422,
 };
 
 const mapStatusError = (type) => {
@@ -12,5 +15,6 @@ const mapStatusError = (type) => {
 
 module.exports = {
   sucessStatus,
+  successfullCreationStatus,
   mapStatusError,
 };
