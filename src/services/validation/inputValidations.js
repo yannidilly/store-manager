007bd0a947a815f3bc67d3b24ed78bdc663ce520
Yanni <yannidilly.prog@gmail.com>
2023-02-26 20:07:28 -0300

    cria tratamento de erro para validação de dados de nova venda

--- src/services/validation/inputValidations.js
+++ src/services/validation/inputValidations.js
@@ -1,4 +1,4 @@
-const { idSchema, newProductSchema } = require('./schemas');
+const { idSchema, newProductSchema, newSaleSchema } = require('./schemas');
 
 const validateProductId = (id) => {
   const { error } = idSchema.validate(id);
@@ -13,7 +13,25 @@ const validateNewProduct = (newProduct) => {
   return { type: 'NAME_LENGTH_ERROR', message: '"name" length must be at least 5 characters long' };
 };
 
+const validateNewSale = (newSale) => {
+  const { error } = newSaleSchema.validate(newSale);
+  if (!error) return { type: null, message: newSale };
+  const someProductsSaleDontHaveProductId = newSale.some((product) => !product.productId);
+  if (someProductsSaleDontHaveProductId) {
+    return { type: 'INVALID_VALUE', message: '"productId" is required' };
+  }
+  const someProductsSaleDontHaveQuantity = newSale.some((product) => !product.quantity);
+  if (someProductsSaleDontHaveQuantity) {
+    return { type: 'INVALID_VALUE', message: '"quantity" is required' };
+  }
+  const someProductSaleQuantityIsNegative = newSale.some((sale) => sale.quantity <= 0);
+  if (someProductSaleQuantityIsNegative) {
+    return { type: 'QUANTITY_MIN_ERROR', message: '"quantity" must be greater than or equal to 1' };
+  }
+};
+
 module.exports = {
   validateProductId,
   validateNewProduct,
+  validateNewSale,
 };
