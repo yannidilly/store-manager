f99d13b8e0e6931ce758472f1582d4bcbf650598
Yanni <yannidilly.prog@gmail.com>
2023-02-26 10:15:43 -0300

    cria testes para função findAll da camada controller de produtos

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -1,7 +1,13 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai; // apenas uma desestruturação para na parte dos testes não ter que escrever chai.expect todas as vezes
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
-const connection = require('../../../src/models/connection');
+const productService = require('../../../src/services/products.service');
+const { allProducts, productId1 } = require('../mocks/products');
+const serviceResponse = require('../mocks/serviceResponses');
+const productController = require('../../../src/controllers/products.controller');
 
 describe('Realiza testes nas funções do produto da camada controller', () => {
 
@@ -10,7 +16,17 @@ describe('Realiza testes nas funções do produto da camada controller', () => {
   })
 
   it('Testa se função findAll retorna lista com todos os produtos', async () => {
+    const req = {};
+    const res = {};
 
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(productService, 'findAll').resolves(serviceResponse.findAll);
+
+    await productController.findAll(req, res);
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(allProducts);
   });
 
   it('Testa se a função findById retorna o objeto com as informações do produto com o id passado', async () => {
