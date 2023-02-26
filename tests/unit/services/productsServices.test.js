d9b7109810364c9908739a81f477e989898d73d4
Yanni <yannidilly.prog@gmail.com>
2023-02-25 22:35:27 -0300

    corrige importação da função productsServices nos testes da camada service

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const sinon = require("sinon");
 
 const connection = require("../../../src/models/connection");
-const productsModel = require('../../../src/models/products.model');
+const productsServices = require('../../../src/services/products.service');
 
 const { allProducts, productId1 } = require('../mocks/products');
 
@@ -15,7 +15,7 @@ describe('Realiza testes nas funções do produto da camada service', () => {
   it('Testa se função findAll retorna lista com todos os produtos', async () => {
     sinon.stub(connection, 'execute').resolves([allProducts]);
 
-    const result = await productsModel.findAll();
+    const result = await productsServices.findAll();
     expect(result.type).to.be.deep.equal(null);
     expect(result.message).to.be.deep.equal(allProducts);
   });
@@ -23,7 +23,7 @@ describe('Realiza testes nas funções do produto da camada service', () => {
   it('Testa se a função findById retorna o objeto com as informações do produto com o id passado', async () => {
     sinon.stub(connection, 'execute').resolves([productId1]);
 
-    const result = await productsModel.findById(1);
+    const result = await productsServices.findById(1);
     expect(result.type).to.be.deep.equal(null);
     expect(result.message).to.be.deep.equal(productId1);
   });
