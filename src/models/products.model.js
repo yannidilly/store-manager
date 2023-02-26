a0fa82da15777067cce98a7842c74663071dd1a0
Yanni <yannidilly.prog@gmail.com>
2023-02-26 11:08:50 -0300

    corrige retorno da função createProduct na camada model

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -21,12 +21,12 @@ const createProduct = async (newProduct) => {
   const columns = Object.keys(snakeize(newProduct)).join(', ');
   const placeholder = Object.keys(newProduct).map((_key) => '?').join(', ');
 
-  const [{ newProductId }] = await connection.execute(
+  const [{ newProductId, name }] = await connection.execute(
     `INSERT INTO StoreManager.products (${columns}) VALUE (${placeholder})`,
     [...Object.values(newProduct)],
   );
 
-  return newProductId;
+  return { id: newProductId, name };
 };
 
 module.exports = {
