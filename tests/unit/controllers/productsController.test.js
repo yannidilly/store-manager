ffb03db3a8b7759d9198ab883761c2029bbb8602
Yanni <yannidilly.prog@gmail.com>
2023-02-25 23:12:03 -0300

    cria estrutura para testes na camada controller

---
+++ tests/unit/controllers/productsController.test.js
@@ -0,0 +1,19 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const connection = require('../../../src/models/connection');
+
+describe('Realiza testes nas funções do produto da camada controller', () => {
+
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  it('Testa se função findAll retorna lista com todos os produtos', async () => {
+
+  });
+
+  it('Testa se a função findById retorna o objeto com as informações do produto com o id passado', async () => {
+ 
+  });
+});
