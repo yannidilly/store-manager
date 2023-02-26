6e3a2318fefe75e3cb26637b92f1127fe08bc2a8
Yanni <yannidilly.prog@gmail.com>
2023-02-25 21:07:38 -0300

    cria teste para função findById na camada service

--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -16,6 +16,10 @@ describe('Realiza testes nas funções do produto da camada service', () => {
   });
 
   test('Testa se a função findById retorna o objeto com as informações do produto com o id passado', async () => {
-    
+    sinon.stub(connection, 'execute').resolves(productId1);
+
+    const result = await productsModel.findById(1);
+    expect(result.type).to.be.deep.equal(null);
+    expect(result.message).to.be.deep.equal(productId1);
   });
 });
