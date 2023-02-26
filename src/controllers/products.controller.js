2ec3fef34724c43ad3e4e0f783da03e72c2ccc92
Yanni <yannidilly.prog@gmail.com>
2023-02-26 10:36:00 -0300

    remove tratamento de erro desnecessário na função findById da camada controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,8 +2,7 @@ const productsService = require('../services/products.service');
 const { sucessStatus, mapStatusError } = require('../utils/mapStatus');
 
 const findAll = async (_req, res) => {
-  const { type, message } = await productsService.findAll();
-  if (type) return res.status(mapStatusError(type)).json(message);
+  const { message } = await productsService.findAll();
   return res.status(sucessStatus).json(message);
 };
 
