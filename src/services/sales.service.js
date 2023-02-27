ae03e685ab3a7becc6c67e179ec3b75582357d76
Yanni <yannidilly.prog@gmail.com>
2023-02-27 00:08:15 -0300

    corrige condição de verificação se produto existe

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -4,8 +4,8 @@ const validate = require('./validation/inputValidations');
 const newSale = async (salesProducts) => {
   const error = validate.validateNewSale(salesProducts);
   if (error.type) return error;
-  const allProductsIdExist = await validate.verificateProductIdExist(salesProducts);
-  if (!allProductsIdExist) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+  const anyProductsIdDontExist = await validate.verificateProductIdExist(salesProducts);
+  if (anyProductsIdDontExist) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
   const sale = await saleModel.newSale(salesProducts);
   return { type: null, message: sale };
 };
