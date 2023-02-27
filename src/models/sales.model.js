1abe698425abec47cc9f8fd05a02f54dad33172a
Yanni <yannidilly.prog@gmail.com>
2023-02-27 11:22:27 -0300

    remove coluna de sale_id da query da função findById

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -11,7 +11,7 @@ const findAll = async () => {
 
 const findById = async (id) => {
   const [sale] = await connection.execute(
-    `SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM StoreManager.sales_products AS sp
+    `SELECT s.date, sp.product_id, sp.quantity FROM StoreManager.sales_products AS sp
     INNER JOIN StoreManager.sales AS s ON sp.sale_id = s.id WHERE sp.sale_id = ?
     ORDER BY sp.sale_id, sp.product_id;`,
     [id],
