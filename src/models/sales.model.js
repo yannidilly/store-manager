2d557f30485628ea5c90688d06c3d414189c465c
Yanni <yannidilly.prog@gmail.com>
2023-02-26 23:37:13 -0300

    corrige query sql e corrige resposta da função newSale da camada model

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,14 +1,27 @@
 const connection = require('./connection');
 
 const newSale = async (productsSale) => {
-  const arrayProductData = productsSale.map((product) => (Object.values(product).join(', ')));
-  const stringProductData = arrayProductData.map((productData) => `(${productData})`).join(', ');
+  const dateList = productsSale.map((_product) => '(NOW())').join(', ');
 
-  const [productSale] = await connection.execute(
-    `INSERT INTO StoreManager.sales_products (productId, quantity) VALUES ${stringProductData};
-    INSERT INTO StoreManager.sales (date) VALUE (NOW());`,
+  const [{ insertId }] = await connection.execute(
+    `INSERT INTO StoreManager.sales (date) VALUES ${dateList};`,
   );
-  return productSale;
+
+  const arrayProductData = productsSale.map((product) => (Object.values(product).join(', ')));
+  const stringProductData = arrayProductData
+      .map((productData) => `(${productData}, ${insertId})`).join(', ');
+
+  await connection.execute(
+    `INSERT INTO StoreManager.sales_products (product_id, quantity, sale_id)
+    VALUES ${stringProductData};`,
+  );
+
+  const response = {
+    id: insertId,
+    itemsSold: productsSale,
+  };
+
+  return response;
 };
 
 module.exports = {
