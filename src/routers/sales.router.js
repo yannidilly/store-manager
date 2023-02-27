e84e3afe0d85d21c2e5e0df58f2310c48365fa65
Yanni <yannidilly.prog@gmail.com>
2023-02-27 10:50:25 -0300

    implementa endpoint /sales para listar todas as vendas

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -3,6 +3,8 @@ const salesController = require('../controllers/sales.controller');
 
 const router = express.Router();
 
+router.get('/', salesController.allSales);
+
 router.post('/', salesController.newSale);
 
 module.exports = router;
