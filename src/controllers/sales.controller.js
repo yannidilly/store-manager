3bc1f420ca9e2b2447eba80b23491b17c06c0c7b
Yanni <yannidilly.prog@gmail.com>
2023-02-26 21:35:43 -0300

    cria função newSale da camada controller

---
+++ src/controllers/sales.controller.js
@@ -0,0 +1,13 @@
+const salesService = require('../services/sales.service');
+const { sucessStatus, mapStatusError } = require('../utils/mapStatus');
+
+const newSale = async (req, res) => {
+  const { salesProducts } = req.body;
+  const { type, message } = await salesService.newSale(salesProducts);
+  if (type) return res.status(mapStatusError(type)).json({ message });
+  return res.status(sucessStatus.created).json(message);
+};
+
+module.exports = {
+  newSale,
+};
