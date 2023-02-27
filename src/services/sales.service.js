1394a6954bc357178bd9797a1a93146f75db6a4d
Yanni <yannidilly.prog@gmail.com>
2023-02-26 21:29:30 -0300

    adiciona validações na função newSale da camada service

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -1,8 +1,11 @@
 const saleModel = require('../models/sales.model');
+const validate = require('./validation/inputValidations');
 
 const newSale = async (salesProducts) => {
-  // inputValidation
-  // productIdValidation
+  const error = validate.validateNewSale(salesProducts);
+  if (error.type) return error;
+  const allProductsIdExist = validate.verificateProductIdExist(salesProducts);
+  if (!allProductsIdExist) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
   const sale = await saleModel.newSale(salesProducts);
   return sale;
 };
