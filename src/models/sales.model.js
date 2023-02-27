6ad65bf83824f549ae15f0353fc788c50cd62e26
Yanni <yannidilly.prog@gmail.com>
2023-02-27 11:03:56 -0300

    cria função findById para sales na camdada model

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -9,31 +9,37 @@ const allSales = async () => {
   return camelize(sales);
 };
 
+const findById = async (id) => {
+  const [sale] = await connection.execute(
+    `SELECT sp.sale_id, s.date, sp.product_id, sp.quantity FROM StoreManager.sales_products AS sp
+    INNER JOIN StoreManager.sales AS s ON sp.sale_id = s.id WHERE sp.sale_id = ?
+    ORDER BY sp.sale_id, sp.product_id;`,
+    [id],
+  );
+  return camelize(sale);
+};
+
 const newSale = async (productsSale) => {
   const dateList = productsSale.map((_product) => '(NOW())').join(', ');
-
   const [{ insertId }] = await connection.execute(
     `INSERT INTO StoreManager.sales (date) VALUES ${dateList};`,
   );
-
   const arrayProductData = productsSale.map((product) => (Object.values(product).join(', ')));
   const stringProductData = arrayProductData
       .map((productData) => `(${productData}, ${insertId})`).join(', ');
-
   await connection.execute(
     `INSERT INTO StoreManager.sales_products (product_id, quantity, sale_id)
     VALUES ${stringProductData};`,
   );
-
   const response = {
     id: insertId,
     itemsSold: productsSale,
   };
-
   return response;
 };
 
 module.exports = {
   allSales,
+  findById,
   newSale,
 };
