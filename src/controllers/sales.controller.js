15e6742a92549352430759cf7340d555ae8033b6
Yanni <yannidilly.prog@gmail.com>
2023-02-26 21:48:42 -0300

    corrige desestruturação do req.body na função newSale da camada controller

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -2,7 +2,7 @@ const salesService = require('../services/sales.service');
 const { sucessStatus, mapStatusError } = require('../utils/mapStatus');
 
 const newSale = async (req, res) => {
-  const { salesProducts } = req.body;
+  const salesProducts = req.body;
   const { type, message } = await salesService.newSale(salesProducts);
   if (type) return res.status(mapStatusError(type)).json({ message });
   return res.status(sucessStatus.created).json(message);
