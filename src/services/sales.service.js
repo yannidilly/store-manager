697ec04105d0dced1a86c6c44f119238606a4553
Yanni <yannidilly.prog@gmail.com>
2023-02-26 21:31:23 -0300

    corrige bug de assincronicidade

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -4,7 +4,7 @@ const validate = require('./validation/inputValidations');
 const newSale = async (salesProducts) => {
   const error = validate.validateNewSale(salesProducts);
   if (error.type) return error;
-  const allProductsIdExist = validate.verificateProductIdExist(salesProducts);
+  const allProductsIdExist = await validate.verificateProductIdExist(salesProducts);
   if (!allProductsIdExist) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
   const sale = await saleModel.newSale(salesProducts);
   return sale;
