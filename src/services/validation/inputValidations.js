3bf24d96ca9e871578154fa24ef51d2386366a63
Yanni <yannidilly.prog@gmail.com>
2023-02-26 23:51:16 -0300

    inverte ordem da validação de tamanho e de existencia do valor porque !0 retorna true

--- src/services/validation/inputValidations.js
+++ src/services/validation/inputValidations.js
@@ -31,13 +31,13 @@ const validateNewSale = (newSale) => {
   if (someProductsSaleDontHaveProductId) {
     return { type: 'INVALID_VALUE', message: '"productId" is required' };
   }
-  const someProductsSaleDontHaveQuantity = newSale.some((product) => !product.quantity);
-  if (someProductsSaleDontHaveQuantity) {
-    return { type: 'INVALID_VALUE', message: '"quantity" is required' };
-  }
   const someProductSaleQuantityIsNegative = newSale.some((sale) => sale.quantity <= 0);
   if (someProductSaleQuantityIsNegative) {
     return { type: 'QUANTITY_MIN_ERROR', message: '"quantity" must be greater than or equal to 1' };
+  }
+  const someProductsSaleDontHaveQuantity = newSale.some((product) => !product.quantity);
+  if (someProductsSaleDontHaveQuantity) {
+    return { type: 'INVALID_VALUE', message: '"quantity" is required' };
   }
 };
 
